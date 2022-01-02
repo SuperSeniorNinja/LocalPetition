aa443799731c2490c7166f7c59978d5d5b187206
Felix Scherf <felixscherf@me.com>
2022-01-02 20:10:02 +0100

    changes with the voting

--- html/static/js/index.js
+++ html/static/js/index.js
@@ -1,3 +1,4 @@
+/* 
 const upvoteempty = '<img style="height: 35px; width: auto;" src="static/img/arrow-up.svg" />';
 const upvotefull = '<img style="height: 35px; width: auto;" src="static/img/arrow-up-fill.svg" />';
 const downvoteempty = '<img style="height: 35px; width: auto;" src="static/img/arrow-down.svg" />';
@@ -59,31 +60,89 @@ function Down() {
     }
 }
 
+*/
+const upvoteempty = "static/img/arrow-up.svg";
+const upvotefull = "static/img/arrow-up-fill.svg";
+const downvoteempty = "static/img/arrow-down.svg";
+const downvotefull = "static/img/arrow-down-fill.svg";
 
+upvotebutton = document.getElementById("upvotebutton");
 
+downvotebutton = document.getElementById("downvotebutton");
 
-function donate() {
-    if (document.getElementById("inputAmount").value !== "") {
-        if (donated === 0) {
-            donated = donated + 1;
-            document.getElementById("Supporters").innerHTML = donated;
-        }
-    }
+upvotecounter = document.getElementById("upvotes");
+
+downvotecounter = document.getElementById("downvotes");
+
+upanddownvotecounter = document.getElementById("votes");
+
+upvotebutton.addEventListener("click", Up);
+
+downvotebutton.addEventListener("click", Down);
+
+function Up() {
+  if (upvotecounter.innerHTML == 0) {
+    upvotecounter.innerHTML = 1;
+    upvotebutton.src = upvotefull;
+    upanddownvotecounter.innerHTML =
+      parseInt(upvotecounter.innerHTML) + parseInt(downvotecounter.innerHTML);
+  } else if (upvotecounter.innerHTML == 1) {
+    upvotecounter.innerHTML = 0;
+    upvotebutton.src = upvoteempty;
+    upanddownvotecounter.innerHTML =
+      parseInt(upvotecounter.innerHTML) + parseInt(downvotecounter.innerHTML);
+  }
+
+  if (downvotecounter.innerHTML == 1) {
+    downvotecounter.innerHTML = 0;
+    downvotebutton.src = downvoteempty;
+    upanddownvotecounter.innerHTML =
+      parseInt(upvotecounter.innerHTML) + parseInt(downvotecounter.innerHTML);
+  }
+}
+
+function Down() {
+  if (downvotecounter.innerHTML == 0) {
+    downvotecounter.innerHTML = 1;
+    downvotebutton.src = downvotefull;
+    upanddownvotecounter.innerHTML =
+      parseInt(upvotecounter.innerHTML) + parseInt(downvotecounter.innerHTML);
+  } else if (downvotecounter.innerHTML == 1) {
+    downvotecounter.innerHTML = 0;
+    downvotebutton.src = downvoteempty;
+    upanddownvotecounter.innerHTML =
+      parseInt(upvotecounter.innerHTML) + parseInt(downvotecounter.innerHTML);
+  }
+
+  if (upvotecounter.innerHTML == 1) {
+    upvotecounter.innerHTML = 0;
+    upvotebutton.src = upvoteempty;
+    upanddownvotecounter.innerHTML =
+    parseInt(upvotecounter.innerHTML) + parseInt(downvotecounter.innerHTML);
+
+  }
 }
 
 
 
-img_array = new Array('static/img/pin.svg','static/img/pin-fill.svg');
+var donated = 0;
+
+function donate() {
+  if (document.getElementById("inputAmount").value !== "") {
+    if (donated === 0) {
+      donated = donated + 1;
+      document.getElementById("Supporters").innerHTML = donated;
+    }
+  }
+}
+
+img_array = new Array("static/img/pin.svg", "static/img/pin-fill.svg");
 var i = 0;
 
 function marked() {
-    i++;
-    document.getElementById("svg").src=img_array[i];
-    if(i==img_array.length-1) {
-        i=-1;
-    }
+  i++;
+  document.getElementById("svg").src = img_array[i];
+  if (i == img_array.length - 1) {
+    i = -1;
+  }
 }
-
-
-
-

469cf687b70793af272e36bc45ab3afb54e63c05
Felix Scherf <contact@matrix.engineer>
2022-04-06 21:43:17 +0200

    come updates with mui.com React libary

--- src/CreateAPetition.js
+++ src/CreateAPetition.js
@@ -1,6 +1,16 @@
 
 import { NavbarBottom } from "./NavbarBottom";
-import { Link } from "react-router-dom";
+
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+
+
+const Input = styled('input')({
+	display: 'none',
+});
 
 
 
@@ -33,14 +43,25 @@ export const CreateAPetition = () => {
 					<p className="Pflichtfeld"  >* = Pflichtfeld</p>
 					<br />
 					<p className="create-titel create-font-size"  >*Titel:</p>
-					<input className="create-titel-input" maxLength="50" required/>
+					<input className="create-titel-input" maxLength="50" required />
 					<br />
 					<br />
 
-					<p className="create-font-size" >Bild/Video:</p>
-					<input type="file" className="create-image" />
-					<br />
-					<br />
+					<p className="create-font-size" >Bild:</p>
+
+
+
+					<label htmlFor="icon-button-file">
+
+						<Input accept="image/*" id="icon-button-file" type="file" />
+						<IconButton size="large" aria-label="upload picture" component="span">
+							<PhotoCamera sx={{ fontSize: 45, color: "#28a745" }} />
+						</IconButton>
+
+					</label>
+
+
+
 
 					<p className="create-font-size" >*Beschreibung:</p>
 					<textarea cols="100" className="create-description-input" rows="10" maxLength="350" required></textarea>
@@ -49,7 +70,7 @@ export const CreateAPetition = () => {
 					<br />
 
 					<p className="create-font-size" >*Standort:</p>
-					<img  src={require('./img/map_create.png')} className="MapCreate" />
+					<img src={require('./img/map_create.png')} className="MapCreate" />
 
 					<br />
 					<br />
@@ -59,7 +80,7 @@ export const CreateAPetition = () => {
 				</form>
 			</div>
 
-			<NavbarBottom classstart="under-navitem-unselected" classsearch="under-navitem-unselected"  classactivity="under-navitem-unselected" classprofile="under-navitem-unselected" />
+			<NavbarBottom classstart="under-navitem-unselected" classsearch="under-navitem-unselected" classactivity="under-navitem-unselected" classprofile="under-navitem-unselected" />
 		</div>
 	)
 
--- src/YourProfile.js
+++ src/YourProfile.js
@@ -2,17 +2,60 @@
 import { NavbarBottom } from "./NavbarBottom";
 import { Link } from "react-router-dom";
 
+import * as React from 'react';
 
+import FormControl from '@mui/material/FormControl';
+
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+
+import InputAdornment from '@mui/material/InputAdornment';
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import TextField from '@mui/material/TextField';
+
+import OutlinedInput from '@mui/material/OutlinedInput';
 
 export const YourProfile = () => {
 
 
+	const [name1, setName1] = React.useState('');
+
+	const [name2, setName2] = React.useState('');
+
+	const [name3, setName3] = React.useState('');
+
+	const [name4, setName4] = React.useState('');
+
+	const handleChange1 = (event) => {
+		setName1(event.target.value);
+	};
+	const handleChange2 = (event) => {
+		setName2(event.target.value);
+	};
+	const handleChange3 = (event) => {
+		setName3(event.target.value);
+	};
+	const handleChange4 = (event) => {
+		setName4(event.target.value);
+	};
+	const theme = createTheme({
+		palette: {
+			success: {
+
+				main: "#28a745",
+			},
+
+		},
+	});
 
 
 	return (
 
 
 		<div >
+
 			<div className="casual-header-div">
 				<Link to="/einstellungen"   >  <img className="settings-image" src={require('./img/settings.svg')} />  </Link>
 				<h4 className="headline headline-profilesettings"> Dein Profil </h4>
@@ -22,25 +65,71 @@ export const YourProfile = () => {
 
 
 			<div className="casual-menu">
-				<p className="profile-name">(Name)<Link to=""><img src={require('./img/edit.svg')} /></Link> </p>
-
-
 				<br />
+				<ThemeProvider theme={theme}>
 
-				<img src={require('./img/profile.png')} className="profile-picture-fullscreen" />
+					<FormControl>
+						<InputLabel color="success" htmlFor="component-outlined">Name</InputLabel>
+						<OutlinedInput
+							color="success"
+							id="component-outlined"
+							value={name1}
+							onChange={handleChange1}
+							label="Name"
+							sx={{ minWidth: "200px" }}
+						/>
+					</FormControl>
+					<br />
+					<br />
+
+					<img src={require('./img/profile.png')} className="profile-picture-fullscreen" />
 
 
-				<br />
-				<Link to=""><img className="margin-bottom" src={require('./img/edit.svg')} /></Link>
+					<br />
+					<Link to=""><img className="margin-bottom" src={require('./img/edit.svg')} /></Link>
+					<br />
+					<br />
+					<div>
+						<TextField
 
-				<br />
-				<span><img className="link-profile" src={require('./img/link.svg')} /><Link className="profile-link" to="">(Link) <img src={require('./img/edit.svg')} /></Link></span>
+							id="outlined-multiline-static"
+							label="Beschreibung"
+							multiline
+							rows={4}
+							sx={{ minWidth: "250px" }}
+							color="success"
+							
 
+						/>
+						<br />
+						<br />
+						<TextField
+							label="Link"
+							size="small"
+							id="outlined-start-adornment"
+							sx={{ m: 1, width: '18ch' }}
+							color="success"
+							InputProps={{
+								startAdornment: <InputAdornment position="start"><img className="link-profile" src={require('./img/link.svg')} /></InputAdornment>,
+							}}
+						/>
 
-				<p>(Profilbeschreibung) <Link to=""> <img src={require('./img/edit.svg')} /></Link></p>
-				<p><img className="location-marker-profile" src={require('./img/location-marker-profile.svg')} />(Standort) <Link to=""> <img src={require('./img/edit.svg')} /></Link></p>
+						<br />
+						<TextField
+							label="Standort"
+							size="small"
+							id="outlined-start-adornment"
+							sx={{ m: 1, width: '18ch' }}
+							color="success"
+							InputProps={{
+								startAdornment: <InputAdornment position="start"><img className="location-marker-profile" src={require('./img/location-marker-profile.svg')} /></InputAdornment>,
+							}}
+						/>
+					</div>
+				</ThemeProvider>
 			</div>
 			<NavbarBottom classstart="under-navitem-unselected" classsearch="under-navitem-unselected" classactivity="under-navitem-unselected" classprofile="under-navitem-selected" />
+
 		</div>
 	)
 
